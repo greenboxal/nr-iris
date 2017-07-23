cc276201295a8fc50d4e997d3ca7bc774e3a11c0
Jonathan Lima <greenboxal@gmail.com>
2017-07-22 22:29:45 -0300

    Add middleware version

--- instrumentation.go
+++ instrumentation.go
@@ -2,11 +2,51 @@ package nriris
 
 import (
 	"github.com/newrelic/go-agent"
+	"net/http"
 
 	iris "gopkg.in/kataras/iris.v6"
 )
 
 const NewRelicTransaction = "__newrelic_transaction__"
+
+type NewRelic struct {
+	App newrelic.Application
+}
+
+func Apply(nr newrelic.Application, app iris.Framework) {
+	app.Adapt(iris.Policies{
+		RouterWrapperPolicy: func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+			txn := nr.StartTransaction("*", w, r)
+
+			defer func() {
+				err := recover()
+
+				if err != nil {
+					switch err := err.(type) {
+					case error:
+						txn.NoticeError(err)
+					default:
+						txn.NoticeError(errWrapper{err})
+					}
+				}
+
+				txn.End()
+			}()
+
+			next(txn, r)
+		},
+	})
+
+	app.UseFunc(func(ctx *iris.Context) {
+		txn := ctx.ResponseWriter.(newrelic.Transaction)
+
+		txn.SetName(ctx.Request.URL.Path)
+
+		ctx.Set(NewRelicTransaction, txn)
+
+		ctx.Next()
+	})
+}
 
 func WrapHandlerFunc(app newrelic.Application, name string, handler iris.HandlerFunc) iris.HandlerFunc {
 	if app == nil {
